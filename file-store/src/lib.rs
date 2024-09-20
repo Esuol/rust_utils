19b93834201c3c8ef4399f3edac0c9d0774b5006
Esuol <berlinenPlus@gmail.com>
2024-09-20 15:21:42 +0800

    feat: file error enum

--- file-store/src/lib.rs
+++ file-store/src/lib.rs
@@ -1,3 +1,4 @@
+use std::error;
 use std::fs::File as StdFile;
 use std::io::Write;
 use std::path::{Path, PathBuf};
@@ -7,3 +8,15 @@ use tempfile::NamedTempFile;
 use uuid::Uuid;
 
 const UPDATE_FILES_PATH: &str = "updates/updates_files";
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("Could not parse file name as utf-8")]
+    CouldNotParseFileNameAsUtf8,
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
+    #[error(transparent)]
+    PersistError(#[from] tempfile::PersistError),
+    #[error(transparent)]
+    UuidError(#[from] uuid::Error),
+}
