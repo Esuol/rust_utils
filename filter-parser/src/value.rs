87c864600a199a0fa378a3f846426c243e283bec
Esuol <berlinenPlus@gmail.com>
2024-10-18 16:28:43 +0800

    fix: word_not_keyword

--- filter-parser/src/value.rs
+++ filter-parser/src/value.rs
@@ -52,3 +52,16 @@ fn quoted_by(quote: char, input: Span) -> IResult<Token> {
         Token::new(input, escaped.then(|| unescape(input, quote))),
     ))
 }
+
+// word           = (alphanumeric | _ | - | .)+    except for reserved keywords
+pub fn word_not_keyword<'a>(input: Span<'a>) -> IResult<Token<'a>> {
+    let (input, word): (_, Token<'a>) =
+        take_while1(is_value_component)(input).map(|(s, t)| (s, t.into()))?;
+    if is_keyword(word.value()) {
+        return Err(nom::Err::Error(Error::new_from_kind(
+            input,
+            ErrorKind::ReservedKeyword(word.value().to_owned()),
+        )));
+    }
+    Ok((input, word))
+}
