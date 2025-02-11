47dda9aa06ca7100f488ec79b5a7387918a406ba
Esuol <berlinenPlus@gmail.com>
2025-02-11 10:14:32 +0800

    feat: update event

--- imagebuffer/src/event.rs
+++ imagebuffer/src/event.rs
@@ -11,3 +11,30 @@ pub enum Event {
 pub trait Observer {
     fn on_notify(&self, event: &Event);
 }
+
+pub struct EventManager {
+    observers: HashMap<Event, Vec<Arc<Mutex<dyn Observer + Send>>>>,
+}
+
+impl EventManager {
+    pub fn new() -> Self {
+        EventManager {
+            observers: HashMap::new(),
+        }
+    }
+
+    pub fn subscribe(&mut self, event: Event, observer: Arc<Mutex<dyn Observer + Send>>) {
+        self.observers
+            .entry(event)
+            .or_insert(Vec::new())
+            .push(observer);
+    }
+
+    pub fn notify(&self, event: &Event) {
+        if let Some(observers) = self.observers.get(event) {
+            for observer in observers {
+                observer.lock().unwrap().on_notify(event);
+            }
+        }
+    }
+}
