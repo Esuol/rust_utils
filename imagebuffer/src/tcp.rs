91335409cc8ffaf01c068d4bdfcea029ff145279
Esuol <berlinenPlus@gmail.com>
2025-03-07 13:40:13 +0800

    feat: event handler

--- imagebuffer/src/tcp.rs
+++ imagebuffer/src/tcp.rs
@@ -645,3 +645,28 @@ pub fn start_client() {
         }
     }
 }
+
+pub fn start_client() {
+    match TcpStream::connect("127.0.0.1:7878") {
+        Ok(mut stream) => {
+            println!("Successfully connected to server");
+
+            let msg = b"Hello, server!";
+            stream.write(msg).unwrap();
+            println!("Sent message: {:?}", msg);
+
+            let mut buffer = [0; 512];
+            match stream.read(&mut buffer) {
+                Ok(_) => {
+                    println!("Received message: {:?}", &buffer[..]);
+                }
+                Err(e) => {
+                    eprintln!("Failed to read from socket: {}", e);
+                }
+            }
+        }
+        Err(e) => {
+            eprintln!("Failed to connect: {}", e);
+        }
+    }
+}
