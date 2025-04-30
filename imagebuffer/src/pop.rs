726cb341da9c034d1caea21a3f230eac39c7b25b
Esuol <berlinenPlus@gmail.com>
2025-04-30 09:52:23 +0800

    feat: pop

--- imagebuffer/src/pop.rs
+++ imagebuffer/src/pop.rs
@@ -97,6 +97,38 @@ impl<T> Stack<T> {
     }
 }
 
+pub struct Stack<T> {
+    elements: Vec<T>,
+}
+
+impl<T> Stack<T> {
+    // 创建一个新的空栈
+    pub fn new() -> Self {
+        Stack {
+            elements: Vec::new(),
+        }
+    }
+
+    // 向栈中添加元素
+    pub fn push(&mut self, item: T) {
+        self.elements.push(item);
+    }
+
+    // 从栈中移除并返回最后一个元素
+    pub fn pop(&mut self) -> Option<T> {
+        self.elements.pop()
+    }
+
+    // 检查栈是否为空
+    pub fn is_empty(&self) -> bool {
+        self.elements.is_empty()
+    }
+
+    // 获取栈的大小
+    pub fn size(&self) -> usize {
+        self.elements.len()
+    }
+}
 fn main() {
     let mut stack = Stack::new();
 
